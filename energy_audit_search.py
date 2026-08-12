aec4c420c841ba5b954a82e667192ccb66c59b58
mty <1138569845@qq.com>
2026-08-12 09:08:10 +0800

    1.删除早期代码存留的Rag Search 文件

--- energy_audit_search.py
+++
@@ -1,8 +0,0 @@
-"""DEPRECATED: This module has moved to rag.energy_audit_search. Import from there instead."""
-import warnings
-from rag.energy_audit_search import *  # noqa: F403
-warnings.warn(
-    "Importing from energy_audit_search is deprecated. "
-    "Use rag.energy_audit_search instead.",
-    DeprecationWarning, stacklevel=2,
-)
